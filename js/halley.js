fb932ee6c33b02277f33605941d4e5b3acb1c326
edsonapf <edsonalves@cc.ci.ufpb.br>
2019-02-14 23:10:09 -0300

    Talvez o final

--- js/halley.js
+++ js/halley.js
@@ -63,9 +63,7 @@ const halleyMethod = (f, x0, tol, iter) => {
     let der2 = math.derivative(der1, variable).toString()
     
     // Converte o erro para valor
-    console.log('tol antes = ' + tol)
     tol = parseFloat(tol).toFixed(20);
-    console.log('tol depois = ' + tol)
     
     // Converte as funções de String para Função JS
     f = stringToFunction(f, variable)
